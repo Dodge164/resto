ffdbecce91df72469adbc2059b95d810793b3610
Dodge164 <dodge164@mail.ru>
2021-07-14 10:53:11 +0400

    Не рабочий массив

--- src/components/menu-list/menu-list.js
+++ src/components/menu-list/menu-list.js
@@ -1,19 +1,30 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import MenuListItem from '../menu-list-item';
-
+import { connect } from 'react-redux';
+import WithRestoService from '../hoc/with-resto-service';
 import './menu-list.scss';
 
 class MenuList extends Component {
+  componentDidMount() {
+    const { RestoService } = this.props;
+    RestoService.getMenuItems().then((res) => console.log(res));
+  }
 
-    render() {
+  render() {
+    const { menuItems } = this.props;
+    return (
+      <ul className="menu__list">
+        {menuItems.map((menuItem) => {
+          return <MenuListItem key={menuItem.id} menuItem={menuItem} />;
+        })}
+      </ul>
+    );
+  }
+}
 
-        return (
-            <ul className="menu__list">
-                <MenuListItem/>
-            </ul>
-        )
-    }
+const mapStateToProps = (state) => {
+  return {
+    menuItems: state.menu,
+  };
 };
-
-
-export default MenuList;
+export default WithRestoService()(connect(mapStateToProps)(MenuList));
